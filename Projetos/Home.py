5fd3fe1ccf42408e1bb0589ae51df1fe26b3a7ed
gustavobastos <gustavo.bastos199822@gmail.com>
2025-01-17 15:08:28 -0300

    home page. Add icons for all pages, a summary and how to use the dashboard

---
+++ Projetos/Home.py
@@ -0,0 +1,29 @@
+import streamlit as stl
+from PIL import Image
+
+stl.set_page_config(page_title='Home', page_icon='🎲')
+
+image_path = 'D:/Estudos/Analise-de-dados/Projetos/logo.png'
+image = Image.open(image_path)
+
+stl.sidebar.image(image, width=120)
+stl.sidebar.markdown('# Cury Company')
+stl.sidebar.markdown('## Fastest Delivery in Town')
+stl.sidebar.markdown('***')
+
+stl.write('# Cury Company Growth Dashboard')
+
+stl.markdown("""
+Growth Dashboard was built to track the growth metrics of Couriers and Restaurants.
+### How to use this Growth Dashboard?
+- Company Overview:
+    - Management View: General behavior metrics.
+    - Tactical View: Weekly growth indicators.
+    - Geographical View: Geolocation insights.
+
+- Courier Overview:
+    - Monitoring weekly growth indicators.
+
+- Restaurant Overview:
+    - Weekly growth indicators for restaurants.
+""")
